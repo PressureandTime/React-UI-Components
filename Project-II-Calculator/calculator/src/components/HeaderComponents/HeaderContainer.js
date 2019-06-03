d14364e931d32a644959914443f5628bdb9558ae
Petar Scepanovic <hiborija@gmail.com>
2019-06-03 16:41:14 +0200

    started working on calculator

---
+++ Project-II-Calculator/calculator/src/components/HeaderComponents/HeaderContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ImageThumbnail from './Imagethumbnail'
+import HeaderTitle from './HeadertTitle'
+import HeaderContent from './HeaderContent'
+
+
+function HeaderContainer(){
+
+    return(
+        <div>
+<ImageThumbnail />
+<HeaderTitle />
+<HeaderContent />
+        </div>
+
+    )
+}
+
+export default HeaderContainer
